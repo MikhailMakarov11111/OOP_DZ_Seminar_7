751516d67d3f7419592ee780cce048cf0d7959f4
Your Name <mikhailmakarov92@gmail.com>
2024-02-29 11:11:26 +0400

    Создан класс view

--- src/main/java/org/example/view/view.java
+++ src/main/java/org/example/view/view.java
@@ -1,4 +1,22 @@
 package org.example.view;
 
+import java.util.Scanner;
+
 public class view {
+    public void choosingCalculator(Scanner scanner) {
+        System.out.println("Добро пожаловать в Калькулятор, для того чтобы \n " +
+                "продолжить выберите действие:\n" +
+                "1. Операции с натуральными числами; \n" +
+                "2. Операции с комплексными числами; \n" +
+                "3. Закрыть приложение");
+    }
+
+    public void operationsCalculator(Scanner scanner) {
+        System.out.println("Выберите операцию которую вы хотите осуществить: \n" +
+                "1. Сложение; \n" +
+                "2. Умножение; \n" +
+                "3. Деление; \n" +
+                "4. Закрыть приложение");
+    }
+
 }

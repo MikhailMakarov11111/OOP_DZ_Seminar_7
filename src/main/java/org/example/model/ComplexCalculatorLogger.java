6cda7f7cfee6d84c9a678fd26c815e1f9d98e613
Your Name <mikhailmakarov92@gmail.com>
2024-03-02 06:12:35 +0400

    Создан классы ComplexCalculatorLogger и NumberCalculatorLogger

---
+++ src/main/java/org/example/model/ComplexCalculatorLogger.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+import java.util.logging.Logger;
+
+public class ComplexCalculatorLogger {
+    private static final Logger logerComp = Logger.getLogger(ComplexCalculatorLogger.class.getName());
+}
---
+++ src/main/java/org/example/model/NumberCalculatorLogger.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+import java.util.logging.Logger;
+
+public class NumberCalculatorLogger {
+    public static final Logger loggerNum = Logger.getLogger(NumberCalculatorLogger.class.getName());
+}
